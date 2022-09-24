48a16444cc0a127fa4f29f544c56d29f25fe55df
Novianaa <noviana.rahmawati80@gmail.com>
2022-09-25 03:52:42 +0700

    try deploy cyclic-2

--- src/model/moviesModel.js
+++ src/model/moviesModel.js
@@ -6,11 +6,11 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const dbQuery = db.query(`SELECT * FROM movies WHERE title LIKE '%${keyword}%' ORDER BY ${orderBy} ${sortBy} LIMIT ? OFFSET ?`, [limit, offset], (err, newResult) => {
         if (err) {
-          reject(new Error(`${err.sqlMessage}`))
+          reject(new Error(`${err.message}`))
         }
         resolve(newResult)
       })
-      // console.log(dbQuery.sql, 'hjkh')
+      console.log(dbQuery.sql, 'hjkh')
     })
   },
   countMovie: () => {
