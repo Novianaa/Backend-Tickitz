6d4a8325a467acea34a8ccf918e46645e8ae37dc
Novianaa <noviana.rahmawati80@gmail.com>
2022-09-25 04:11:01 +0700

    try deploy

--- src/model/scheduleModel.js
+++ src/model/scheduleModel.js
@@ -50,7 +50,7 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const dbQuery = db.query(`SELECT s.id, s.movie_id, m.title, s.cinema, p.county, s.start_date, s.end_date, s.time, s.price, m.cover, m.categories FROM schedule s LEFT OUTER JOIN movies m ON m.id=s.movie_id JOIN place AS p ON p.id=s.location WHERE start_date <= '${today}%' AND end_date >= '${today}%' AND title LIKE '%${keyword}%' ORDER BY ${orderBy} ${sortBy} LIMIT ? OFFSET ?`, [limit, offset], (err, result) => {
         if (err) {
-          reject(new Error(`${err.sqlMessage}`))
+          reject(new Error(`${err.message}`))
         }
         resolve(result)
       })
