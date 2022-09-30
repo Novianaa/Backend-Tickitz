417113c4b76ada832e1a8ceca9c16b3d8043f89d
Novianaa <noviana.rahmawati80@gmail.com>
2022-09-30 18:34:13 +0700

    update seat

--- src/model/seatBookingModel.js
+++ src/model/seatBookingModel.js
@@ -3,12 +3,13 @@ const db = require('../helpers/mysql')
 module.exports = {
   getSeatBooking: (movie_id, schedule_id, date, time) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT seat FROM seatBooking WHERE movie_id=${movie_id} AND schedule_id=${schedule_id} AND date=${date} AND time=${time}`, (err, result) => {
+      const dbQuery = db.query(`SELECT seat FROM booking_seat WHERE movie_id=${movie_id} AND schedule_id=${schedule_id} AND date='${date}' AND time='${time}'`, (err, result) => {
         if (err) {
           reject(new Error(`${err.message}`))
         }
         resolve(result)
       })
+      console.log(dbQuery.sql, 'as')
     })
   },
   postSeatBooking: (data) => {
