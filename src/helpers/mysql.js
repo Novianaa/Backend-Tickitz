1a9a1acb5a99dfc2026926937d54b8a2250d9e52
Novianaa <noviana.rahmawati80@gmail.com>
2022-09-30 23:28:44 +0700

    mysql

--- src/helpers/mysql.js
+++ src/helpers/mysql.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
 const mysql = require('mysql2')
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env
-const connection = mysql.createConnection({
+const Connection = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
 })
-connection.connect((err) => {
+Connection.connect((err) => {
   if (err) {
     console.log(err)
   }
   console.log('connected to db mysql')
 })
 
-module.exports = connection
+module.exports = Connection
